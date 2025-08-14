ea5f3d7026cdc54845a333ec05f91076a604f442
Clément Delzotti <clement.delzotti@uclouvain.be>
2025-08-14 08:44:10 +0000

    Changing mailbox for uncore

--- controller.c
+++ controller.c
@@ -16,14 +16,15 @@ enum METHODS {
 
 int main(int argc, char const *argv[]){
     if (argc < 2 || strcmp(argv[argc - 1], "--help") == 0) {
-        printf("Usage: %s set/get <frequency>(optional) --debug(optional)\n", argv[0]);
-        printf("  <frequency> : Boost frequency in MHz (required for set)\n");
+        printf("Usage: %s <socket_id> set/get <DF_Pstate [0-2]>(optional) --debug(optional)\n", argv[0]);
+        printf("  <DF_Pstate> : P-state to be imposed to the socket (required for set)\n");
         printf("  --debug     : Enable debug output, prints the actual response from the HSMP message\n");
-        printf("Example:  - %s set 3500\n", argv[0]);
-        printf("          - %s get\n", argv[0]);
+        printf("Example:  - %s 0 set 2\n", argv[0]);
+        printf("          - %s 1 get\n", argv[0]);
         exit(1);
     }
     int file;
+    int socket_id = atoi(argv[1]);
     // Open file exposed by kernel module
     file = open("/dev/hsmp", O_RDWR);
     if (file < 0) {
@@ -32,31 +33,34 @@ int main(int argc, char const *argv[]){
     }
     // Prepare HSMP message structure
     struct hsmp_message msg;
-    msg.sock_ind = 0;
+    msg.sock_ind = socket_id;
     for (int i = 0; i < 8; i++) {
         msg.args[i] = 0;
     }
     // Prepare for set or get
     enum METHODS method; // 0 for set, 1 for get
-    if (strcmp(argv[1], "set") == 0) {
+    if (strcmp(argv[2], "set") == 0) {
         method = SET;
-        msg.msg_id = HSMP_SET_BOOST_LIMIT_SOCKET;
+        msg.msg_id = HSMP_SET_DF_PSTATE;
         msg.num_args = 1;
         msg.response_sz = 0; // No response expected for a SET operation
-        if (argc < 3) {
-            printf("Usage: %s set <frequency>\n", argv[0]);
+        if (argc < 4) {
+            printf("Usage: <socket_id> %s set <frequency>\n", argv[0]);
             exit(1);
         }
-        // Convert frequency argument to integer
-        msg.args[0] = atoi(argv[2]);
-    } else if (strcmp(argv[1], "get") == 0) {
+        // Convert argument to integer
+        msg.args[0] = atoi(argv[3]);
+    } else if (strcmp(argv[2], "get") == 0) {
         method = GET;
-        msg.msg_id = HSMP_GET_BOOST_LIMIT;
+        msg.msg_id = HSMP_GET_FCLK_MCLK;
+        msg.num_args = 0;
+        msg.response_sz = 2;
+    } else if (strcmp(argv[2], "hardcoded") == 0) {
+        method = SET;
+        msg.msg_id = HSMP_SET_AUTO_DF_PSTATE;
         msg.num_args = 1;
-        msg.response_sz = 1;
-    } else {
-        printf("Invalid command. Use 'set' or 'get'.\n");
-        exit(1);
+        msg.response_sz = 0;
+        msg.args[0] = atoi(argv[3]);
     }
     // Send ioctl command to the driver
     int ret = ioctl(file, HSMP_IOCTL_CMD, &msg);
@@ -69,7 +73,8 @@ int main(int argc, char const *argv[]){
     }
     if (method == GET) {
         // Print the response
-        printf("Current Boost Limit: %" PRIu32 " MHz\n", msg.args[0]);
+        printf("Current FCLK: %" PRIu32 " MHz\n", msg.args[0]);
+        printf("Current MCLK: %" PRIu32 " MHz\n", msg.args[1]);
     }
     // Debug option, only if the last argument is "debug"
     if (strcmp(argv[argc - 1], "--debug") == 0) {
